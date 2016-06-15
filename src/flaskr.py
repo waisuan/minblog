174b90ed9703534770d941b5226a4feee003ef88
WSSIA <thewaisuan@gmail.com>
2016-06-15 22:21:33 +0100

    ...

--- src/flaskr.py
+++ src/flaskr.py
@@ -86,7 +86,7 @@ def add_entry():
 
 def filter_entry_text(entry_text):
     regexp = re.compile(r'<div>')
-    if regexp.search(entry_text) is not None:
+    if regexp.search(entry_text) is not None and not entry_text.startswith('<div>'):
         entry_text = '<div>' + re.sub('<div>', '</div><div>', entry_text, 1)
     return entry_text
 
